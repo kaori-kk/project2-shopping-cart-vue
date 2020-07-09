06dc94c4bca8de55aa6a0b44d457178877c9db8c
kaori kohama <kkaori131415@gmail.com>
2020-07-09 17:35:33 +0900

    changed the style of app and added product.json file

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="ro">
+      <div class="col-md-7"></div>
+      <div class="col-md-5"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: #FBF8F3;
+  }
+
 </style>
